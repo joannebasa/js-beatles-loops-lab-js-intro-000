1316ef376b4d050adf95d420a3305f0f7d3d3a35
Joanne Basa <joannegbasa@gmail.com>
2017-11-02 09:30:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ var iLoveTheBeatles = i => {
   while (i < 15);
   return array;
 };
+
+var theBeatlesPlay => (musicians, instruments) {
+  var beatles = [];
+  for(var i = 0; i < musicians.length; i++){
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return beatles
+}
+
+function johnLennonFacts(facts) {
+  var array = [];
+  var i = 0;
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`)
+    i++;
+  }
+  return array
+}
