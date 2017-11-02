2f61e716aaf22a5b53b7144d3f48a2169f3e2c59
Joanne Basa <joannegbasa@gmail.com>
2017-11-02 09:13:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ iLoveTheBeatles = number =>
   var love = [];
 
   var i = 0;
+  var text = "Ilove the Beatles!";
     do {
       text += "The number is " + i;
       i++;
   }
-  while (i <= 15);
+  while (i < 15);
+  love.push(text);
